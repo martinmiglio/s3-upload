077ee43c9c4fa50e296773863bce008524457558
Martin Miglio <marmig0404@gmail.com>
2023-06-17 20:38:54 -0400

    update for aws sdk v3

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 const core = require("@actions/core");
 const fs = require("fs");
-const AWS = require("aws-sdk");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const glob = require("glob");
 
 try {
   const AWS_BUCKET_NAME = core.getInput("AWS_BUCKET_NAME");
   const PATTERN = core.getInput("PATTERN");
-  const DEST = core.getInput("DEST");
+  let DEST = core.getInput("DEST");
+  if (!DEST.endsWith("/")) {
+    DEST += "/";
+  }
   const AWS_SECRET_KEY = core.getInput("AWS_SECRET_KEY");
   const AWS_SECRET_ID = core.getInput("AWS_SECRET_ID");
   const AWS_REGION = core.getInput("AWS_REGION");
   console.log(`Updating Bucket ${AWS_BUCKET_NAME} with ${PATTERN}!`);
-  const s3 = new AWS.S3({
-    apiVersion: "2006-03-01",
-    accessKeyId: AWS_SECRET_ID,
-    secretAccessKey: AWS_SECRET_KEY,
+  const s3 = new S3Client({
     region: AWS_REGION,
+    credentials: {
+      accessKeyId: AWS_SECRET_ID,
+      secretAccessKey: AWS_SECRET_KEY,
+    },
   });
-  const glob = require("glob");
 
   const files = glob.sync(PATTERN);
 
   if (files.length === 0) {
-    core.setOutput("uploaded", false);
-    core.setFailed(`No files found for pattern ${PATTERN}`);
+    throw new Error(`No files found matching ${PATTERN}`);
   }
-
-  const params = {
-    Bucket: AWS_BUCKET_NAME,
-  };
 
   files.forEach((file) => {
     const body = fs.readFileSync(file);
-    params.Body = body;
-    params.Key =
-      DEST + (DEST.endsWith("/") ? "" : "/") + file.replace(/\\/g, "/");
-    s3.upload(params, (err, data) => {
-      if (err) {
+    const params = {
+      Bucket: AWS_BUCKET_NAME,
+      Key: DEST + file.replace(/\\/g, "/"),
+      Body: body,
+    };
+    s3.send(new PutObjectCommand(params))
+      .then(() => {
+        console.log(`Successful upload of ${file} to ${AWS_BUCKET_NAME}`);
+        core.setOutput("uploaded", true);
+      })
+      .catch((err) => {
         console.log(`Failed upload of ${file} to ${AWS_BUCKET_NAME}`);
         throw err;
-      } else {
-        console.log(`Succesful upload of ${file} to ${AWS_BUCKET_NAME}`);
-      }
-    });
-    core.setOutput("uploaded", true);
+      });
   });
 } catch (error) {
   core.setOutput("uploaded", false);
